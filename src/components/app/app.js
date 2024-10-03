5d6b262e9ebd00f619a2fa9c09b346711db18342
Maxim Yakushin <hamonryemax@gmail.com>
2024-10-03 17:54:05 +0200

    add functionality to item-status-filter.

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -19,6 +19,7 @@ export default class App extends Component {
             this.creatTodoItem('Have a lunch')
         ],
         term: '',
+        filter: 'all',
     };
 
     creatTodoItem(label) {
@@ -95,6 +96,10 @@ export default class App extends Component {
         this.setState({ term });
     }
 
+    onFilterChange = (filter) => {
+        this.setState({ filter });
+    }
+
     search(items, term) {
         if (term.length === 0) {
             return items;
@@ -106,13 +111,30 @@ export default class App extends Component {
         });
     }
 
+    filter(items, filter) {
+
+        switch(filter) {
+            case 'all':
+                return items;
+            case 'active':
+                return items.filter((item) => !item.done);
+            case 'done':
+                return items.filter((item) => item.done);
+            default:
+                return items;
+        }
+    }
+
     render() {
 
-        const { todoData, term} = this.state;
+        const { todoData, term, filter } = this.state;
 
-        const visibleItems = this.search(todoData, term);
+        const visibleItems = this.filter(
+            this.search(todoData, term), filter);
+
         const doneCount = todoData
                                  .filter((el) => el.done).length;
+
         const todoCount = todoData.length - doneCount;
 
         return (
@@ -120,8 +142,10 @@ export default class App extends Component {
                 <AppHeader toDo={todoCount} done={doneCount} />
                 <div className="top-panel d-flex">
                     <SearchPanel
-                        onSearchChange={this.onSearchChange}/>
-                    <ItemStatusFilter />
+                        onSearchChange={this.onSearchChange} />
+                    <ItemStatusFilter
+                        filter={filter}
+                        onFilterChange={this.onFilterChange} />
                 </div>
 
                 <TodoList
--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import './search-panel.css';
 
+
 export default class SearchPanel extends Component {
 
     state = {
